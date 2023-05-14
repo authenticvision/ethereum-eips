4ba65cdd7e840906d35c487b57202170cc0e7ef4
Thomas Bergmueller <thomas.bergmueller@authenticvision.com>
2023-05-14 11:43:27 +0200

    Add contract-size and gas-reporting to hardhat test

--- assets/eip-6956/hardhat.config.ts
+++ assets/eip-6956/hardhat.config.ts
@@ -1,17 +1,36 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import '@primitivefi/hardhat-dodoc';
+
+require('hardhat-contract-sizer');
 
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.18",
     settings: {
-      // viaIR: true, // needed for stack depth..
       optimizer: {
         enabled: true,
-        runs: 1
+        runs: 1,
       },
-    },
+    }
   },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
+  },
+  gasReporter: {
+    enabled: true,
+    token: 'ETH',
+    coinmarketcap: '<insertYourKeyHere>'
+  },
+  dodoc: {
+    path: './docs',
+    debugMode: false,
+    runOnCompile: true,
+    
+  }
 };
 
 export default config;
